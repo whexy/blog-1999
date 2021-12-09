0a7640885758aedcd131369282c8a9e52a3b2dcf
WenxuanShi <whexy@outlook.com>
2021-12-09 09:40:33 +0800

    change to Image component

--- components/homepage/WelcomeCard.js
+++ components/homepage/WelcomeCard.js
@@ -1,16 +1,17 @@
+import Image from "next/image";
+import hello from "../../public/img/hello.webp";
+
 export default function WelcomeCard() {
   return (
     <>
-      <div className="max-w-3xl mx-auto bg-cover bg-mojave dark:bg-mojave_dark md:h-[272px] rounded-xl w-full mt-10 overflow-hidden">
-        <div className="grid md:grid-cols-3 mx-auto">
-          <div className="md:col-span-1 grid place-items-center overflow-hidden">
-            <img
-              className="relative w-1/2 md:w-auto h-auto md:h-[240px] rounded-b-full md:rounded-none md:mt-[32px] overflow-hidden object-cover"
-              alt="me waving"
-              src="/img/hello.webp"
-            />
+      <div className="max-w-3xl mx-auto bg-cover bg-mojave dark:bg-mojave_dark sm:h-[272px] rounded-xl w-full mt-10 overflow-hidden">
+        <div className="grid sm:grid-cols-3 mx-auto">
+          <div className="sm:col-span-1 grid place-items-center overflow-hidden">
+            <div className="relative sm:mt-[32px] overflow-hidden">
+              <Image className="rounded-b-full sm:rounded-none" width={194} height={240} alt="me waving" src={hello} />
+            </div>
           </div>
-          <div className="md:col-span-2 place-self-center px-2 md:px-5 rounded-xl relative flex items-center leading-5 mx-1 my-3 md:ml-0 md:mr-3 md:pb-0 backdrop-blur-lg backdrop-brightness-50 select-none">
+          <div className="sm:col-span-2 place-self-center px-2 sm:px-5 rounded-xl relative flex items-center leading-5 mx-1 my-3 sm:ml-0 sm:mr-3 sm:pb-0 backdrop-blur-lg backdrop-brightness-50 select-none">
             <div>
               <div className="py-4 text-white-readable flex flex-col space-y-2 overflow-ellipsis">
                 <p>
