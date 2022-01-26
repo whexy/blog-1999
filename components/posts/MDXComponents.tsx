00b9c6405646bd4c979fb45d0454ce1e910f1198
WenxuanShi <whexy@outlook.com>
2022-01-26 20:09:07 +0800

    post: add post "wasm-rv-emu"

--- components/posts/MDXComponents.tsx
+++ components/posts/MDXComponents.tsx
@@ -2,8 +2,11 @@ import Callout from "@/components/posts/Callout";
 import { Dialog, DialogBack } from "@/components/posts/Dialog";
 import ImgComponent from "@/components/posts/ImgComponent";
 import QuoteComponent from "@/components/posts/QuoteComponent";
-import AnimatedFancyCard from "@/components/AnimatedFancyCard";
 import GithubRepo from "@/components/GithubRepo";
+
+// Special Components
+import AnimatedFancyCard from "@/components/AnimatedFancyCard";
+import WasmRiscvEmu from "@/components/posts/projects/wasm-riscv-emu";
 
 const MDXComponents = {
   img: ImgComponent,
@@ -11,8 +14,9 @@ const MDXComponents = {
   Callout,
   Dialog,
   DialogBack,
-  AnimatedFancyCard, // 《个人博客搭建指北》中使用
   GithubRepo,
+  AnimatedFancyCard, // 《个人博客搭建指北》中使用
+  WasmRiscvEmu, // 《WASM RISC-V 模拟器》中使用
 };
 
 export default MDXComponents;
