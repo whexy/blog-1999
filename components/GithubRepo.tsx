d3c73516fc837eab2cb9abfa7a793d49b8e10a73
WenxuanShi <whexy@outlook.com>
2022-01-26 11:43:34 +0800

    feat: GithubRepo component with CSR

--- components/GithubRepo.tsx
+++ components/GithubRepo.tsx
@@ -1,48 +1,67 @@
-import useSWR from "swr";
 import Image from "next/image";
 import Link from "next/link";
-import fetcher from "@/lib/fetcher";
+import { useState, useEffect } from "react";
 import { StarIcon, UserIcon } from "@heroicons/react/outline";
 
 const GithubRepo = ({ repo }: { repo: string }) => {
-  const RepoEndpoint = `https://api.github.com/repos/` + repo;
-  const { data } = useSWR(RepoEndpoint, fetcher);
-  const { name, description, html_url, owner, stargazers_count } =
-    data;
-  const { login, avatar_url } = owner;
+  // should be a pure client rendered component
+  const [data, setData] = useState(null);
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    const RepoEndpoint = `https://api.github.com/repos/` + repo;
+    fetch(RepoEndpoint)
+      .then(res => res.json())
+      .then(data => {
+        setData(data);
+        if ("name" in data) setIsLoading(false);
+      });
+  }, [repo]);
 
   return (
-    <div className="not-prose max-w-xl mx-auto">
-      <Link href={html_url}>
-        <a>
-          <div className="p-4 secondbg border rounded-xl flex">
-            <div className="grid place-items-center">
-              <Image
-                src={avatar_url}
-                alt={login}
-                height={100}
-                width={100}
-              />
-            </div>
-            <div className="flex flex-col justify-between">
-              <div>
-                <p className="font-semibold text-lg">{name}</p>
-                <p className="font-light text-sm">{description}</p>
-              </div>
-              <div className="flex space-x-4 text-sm">
-                <div className="flex space-x-1 items-center">
-                  <UserIcon className="w-4 h-4" />
-                  <p>{login}</p>
+    <div>
+      {isLoading ? (
+        <Link href={`https://github.com/${repo}`}>
+          <a>{`https://github.com/${repo}`}</a>
+        </Link>
+      ) : (
+        <div className="not-prose max-w-xl mx-auto">
+          <Link href={data.html_url}>
+            <a>
+              <div className="p-4 secondbg border rounded-xl flex">
+                <div className="grid place-items-center">
+                  <Image
+                    src={data.owner.avatar_url}
+                    alt={data.owner.login}
+                    height={100}
+                    width={100}
+                  />
                 </div>
-                <div className="flex space-x-1 items-center">
-                  <StarIcon className="w-4 h-4" />
-                  <p>{stargazers_count}</p>
+                <div className="flex flex-col justify-between">
+                  <div>
+                    <p className="font-semibold text-lg">
+                      {data.name}
+                    </p>
+                    <p className="font-light text-sm">
+                      {data.description}
+                    </p>
+                  </div>
+                  <div className="flex space-x-4 text-sm">
+                    <div className="flex space-x-1 items-center">
+                      <UserIcon className="w-4 h-4" />
+                      <p>{data.owner.login}</p>
+                    </div>
+                    <div className="flex space-x-1 items-center">
+                      <StarIcon className="w-4 h-4" />
+                      <p>{data.stargazers_count}</p>
+                    </div>
+                  </div>
                 </div>
               </div>
-            </div>
-          </div>
-        </a>
-      </Link>
+            </a>
+          </Link>
+        </div>
+      )}
     </div>
   );
 };
