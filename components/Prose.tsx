168b86d1aaf1ce14666dd2e7c68fc90c11c44643
WenxuanShi <whexy@outlook.com>
2022-02-03 16:42:26 +0800

    styling: new href link style

--- components/Prose.tsx
+++ components/Prose.tsx
@@ -1,6 +1,6 @@
 const Prose = ({ children }) => {
   return (
-    <div className="prose md:prose-lg dark:prose-invert prose-slate mx-2 sm:mx-auto overscroll-contain prose-headings:font-sans prose-a:font-semibold prose-a:text-indigo-700 dark:prose-a:text-indigo-300">
+    <div className="prose md:prose-lg dark:prose-invert prose-slate mx-2 sm:mx-auto overscroll-contain prose-headings:font-sans prose-a:text-red-700 dark:prose-a:text-red-300">
       {children}
     </div>
   );
