257e4a540960132d8c01479918fd09a6fe86e18f
WenxuanShi <whexy@outlook.com>
2022-02-03 17:53:16 +0800

    feat: add Skeleton

--- components/GithubRepo.tsx
+++ components/GithubRepo.tsx
@@ -3,52 +3,56 @@ import Link from "next/link";
 import { StarIcon, UserIcon } from "@heroicons/react/outline";
 import useSWR from "swr";
 import fetcher from "@/lib/fetcher";
+import Skeleton from "@/components/Skeleton";
 
 const GithubRepo = ({ repo }: { repo: string }) => {
-  const { data } = useSWR(
-    `/api/github-repo?repo=${repo}`,
-    fetcher,
-    {
-      fallbackData: {
-        html_url: `https://github.com/${repo}`,
-        owner: { avatar_url: "", login: repo.split("/")[0] },
-        name: repo.split("/")[1],
-        description: "Loading...",
-        stargazers_count: "unknown",
-      },
-    },
-  );
+  const html_url = `https://github.com/${repo}`;
+  const username = repo.split("/")[0];
+  const repo_name = repo.split("/")[1];
+
+  const { data } = useSWR(`/api/github-repo?repo=${repo}`, fetcher);
+
+  // we need `data.owner.avatar_url`, `data.description`,
+  // and `data.stargazers_count`
 
   return (
     <div className="not-prose max-w-xl mx-auto">
-      <Link href={data.html_url}>
+      <Link href={html_url}>
         <a className="group">
           <div className="p-4 secondbg border rounded-xl flex space-x-4">
             <div className="flex-none grid place-items-center">
-              {data.owner.avatar_url && <Image
-                src={data.owner.avatar_url}
-                alt={data.owner.login}
-                height={60}
-                width={60}
-              />}
+              {data && data.owner ? (
+                <Image
+                  src={data.owner.avatar_url}
+                  alt={username}
+                  height={60}
+                  width={60}
+                />
+              ) : (
+                <Skeleton height={60} width={60} />
+              )}
             </div>
             <div className="flex flex-col justify-between space-y-1">
               <div>
                 <p className="font-semibold text-lg group-hover:underline">
-                  {data.name}
+                  {repo_name}
                 </p>
                 <p className="font-light text-sm">
-                  {data.description}
+                  {(data && data.description) || <Skeleton />}
                 </p>
               </div>
               <div className="flex space-x-4 text-sm">
                 <div className="flex space-x-1 items-center">
                   <UserIcon className="w-4 h-4" />
-                  <p>{data.owner.login}</p>
+                  <p>{username}</p>
                 </div>
                 <div className="flex space-x-1 items-center">
                   <StarIcon className="w-4 h-4" />
-                  <p>{data.stargazers_count}</p>
+                  <p>
+                    {(data && data.stargazers_count) || (
+                      <Skeleton width={10} />
+                    )}
+                  </p>
                 </div>
               </div>
             </div>
