b73d2a88629a3b910ae335767182577713093e09
WenxuanShi <whexy@outlook.com>
2022-02-10 00:13:23 +0800

    feat: add icon to Callout component

--- components/posts/Callout.tsx
+++ components/posts/Callout.tsx
@@ -1,11 +1,20 @@
-const Callout = ({ title, children }) => {
+const Callout = ({ icon, title, children }) => {
   return (
-    <div className="callout secondbg mx-auto break-inside-avoid-page rounded-lg px-4 py-1 sm:w-11/12">
-      <div className="prose-sm px-4 py-1 dark:prose-invert md:prose">
-        <p className="font-sans text-[1.1em] font-semibold">
-          {title}
-        </p>
-        {children}
+    <div className="callout secondbg relative mx-auto break-inside-avoid-page rounded-lg px-4 py-1 md:w-11/12">
+      {icon && (
+        <span className="absolute top-0 right-2 text-[80px] opacity-20">
+          {icon}
+        </span>
+      )}
+      <div className="relative z-20 px-4 py-1">
+        <div className="not-prose">
+          <p className="my-1 font-sans text-base font-semibold md:-mb-2 md:mt-4 md:text-lg">
+            {title}
+          </p>
+        </div>
+        <div className="prose-sm dark:prose-invert md:prose">
+          {children}
+        </div>
       </div>
     </div>
   );
