93f1072334b168807ad499748057ce269e1ce729
WenxuanShi <whexy@outlook.com>
2022-02-22 13:51:51 +0800

    more friends

--- data/friends.ts
+++ data/friends.ts
@@ -4,7 +4,6 @@ interface Friend {
   icon: string;
   /** URL of blog website */
   url: string;
-  display_url: string;
   description: string;
 }
 
@@ -13,49 +12,78 @@ const friends: Friend[] = [
     name: "macromogic",
     icon: "https://avatars.githubusercontent.com/u/20332455?v=4",
     url: "https://blog.macromogic.xyz/",
-    display_url: "blog.macromogic.xyz",
     description: "",
   },
   {
     name: "NekoDaemon",
     icon: "https://avatars.githubusercontent.com/u/29330054?v=4",
     url: "https://nekodaemon.com/",
-    display_url: "nekodaemon.com",
     description: "",
   },
   {
     name: "mstmoonshine",
     icon: "https://avatars.githubusercontent.com/u/13410629?v=4",
     url: "https://mstmoonshine.github.io/",
-    display_url: "mstmoonshine.github.io",
     description: "",
   },
   {
     name: "Tert-butyllithium",
     icon: "https://avatars.githubusercontent.com/u/40877238?v=4",
     url: "https://blog.lanran.work/",
-    display_url: "blog.lanran.work",
     description: "",
   },
   {
     name: "Gogo",
     icon: "https://avatars.githubusercontent.com/u/12986936?v=4",
     url: "https://blog.gogo.moe/",
-    display_url: "blog.gogo.moe",
     description: "",
   },
   {
     name: "gdjs2",
     icon: "https://avatars.githubusercontent.com/u/17545585?v=4",
     url: "https://gdjs2.cn/",
-    display_url: "gdjs2.cn",
     description: "",
   },
   {
     name: "Arpels",
     icon: "https://avatars.githubusercontent.com/u/35730935?v=4",
     url: "http://blog.arpe1s.xyz/",
-    display_url: "blog.arpe1s.xyz",
+    description: "",
+  },
+  {
+    name: "Neko",
+    icon: "https://avatars.githubusercontent.com/u/39666438?v=4",
+    url: "https://eveneko.com/",
+    description: "",
+  },
+  {
+    name: "Sparktour",
+    icon: "https://avatars.githubusercontent.com/u/14960340?v=4",
+    url: "https://sparktour.me/",
+    description: "",
+  },
+  {
+    name: "Frankss",
+    icon: "https://avatars.githubusercontent.com/u/56958034?v=4",
+    url: "https://sust.icu/",
+    description: "",
+  },
+  {
+    name: "lethal",
+    icon: "https://avatars.githubusercontent.com/u/47763046?v=4",
+    url: "https://lethal233.github.io/",
+    description: "",
+  },
+  {
+    name: "jwnhy",
+    icon: "https://avatars.githubusercontent.com/u/18240025?v=4",
+    url: "https://jwnhy.github.io/",
+    description: "",
+  },
+  {
+    name: "Luyu",
+    icon: "https://avatars.githubusercontent.com/u/2239547?v=4",
+    url: "https://luyu.blog/",
     description: "",
   },
 ];
--- pages/friends.tsx
+++ pages/friends.tsx
@@ -1,49 +1,60 @@
 import Image from "next/image";
 import friends from "@/data/friends";
-import Main from "@/components/Main";
+import GithubRepo from "@/components/GithubRepo";
 
 const FriendPage = () => {
   return (
-    <Main>
-      <div className="mx-3 grid grid-cols-1 gap-3 py-10 sm:grid-cols-2">
-        {friends
-          .sort((f1, f2) => f1.name.localeCompare(f2.name))
-          .map(friend => (
-            <div
-              key={friend.name}
-              className="secondbg group relative flex flex-row items-center justify-center gap-5 rounded-lg py-8"
-            >
-              <div className="absolute right-4 top-4 h-1 w-1 bg-{{friend.status}}-600 dark:bg-{{friend.status}}-300 rounded-full"></div>
-              <div className="ml-8 h-16 w-16 overflow-hidden rounded-full border-2 border-gray-700 transition-all group-hover:scale-105 dark:border dark:border-white/10">
-                <Image
-                  src={friend.icon}
-                  alt={friend.name}
-                  className=""
-                  width={64}
-                  height={64}
-                  placeholder="blur"
-                  blurDataURL="/img/smile.svg"
-                />
+    <div className="bg-white text-black-readable dark:bg-black-readable dark:text-white-readable">
+      <div className="mx-auto max-w-6xl">
+        <h2 className="pt-10 pb-2 text-center text-xl font-light">
+          My Friends
+        </h2>
+        <div className="mx-3 grid grid-cols-1 gap-3 py-5 sm:grid-cols-2 lg:grid-cols-3">
+          {friends
+            .sort((f1, f2) => f1.name.localeCompare(f2.name))
+            .map(friend => (
+              <div
+                key={friend.name}
+                className="secondbg group relative flex flex-row items-center justify-center gap-5 rounded-lg py-8"
+              >
+                <div className="ml-8 h-16 w-16 overflow-hidden rounded-full transition-all group-hover:scale-105 ">
+                  <Image
+                    src={friend.icon}
+                    alt={friend.name}
+                    className=""
+                    width={64}
+                    height={64}
+                    placeholder="blur"
+                    blurDataURL="/img/smile.svg"
+                  />
+                </div>
+                <div className="grow">
+                  <p className="font-mono text-xl">{friend.name}</p>
+                  <p className="w-full truncate text-sm text-jbgray-light transition-all group-hover:text-blue-600 dark:group-hover:text-blue-300">
+                    <a
+                      href={friend.url}
+                      target="_blank"
+                      rel="noreferrer"
+                    >
+                      {friend.url.split("/")[2]}
+                    </a>
+                  </p>
+                  <p className="pt-2 text-sm text-jbgray-light">
+                    {friend.description}
+                  </p>
+                </div>
               </div>
-              <div className="grow">
-                <p className="font-mono text-xl">{friend.name}</p>
-                <p className="w-full truncate text-sm text-jbgray-light transition-all group-hover:text-blue-600 dark:group-hover:text-blue-300">
-                  <a
-                    href={friend.url}
-                    target="_blank"
-                    rel="noreferrer"
-                  >
-                    {friend.display_url}
-                  </a>
-                </p>
-                <p className="pt-2 text-sm text-jbgray-light">
-                  {friend.description}
-                </p>
-              </div>
-            </div>
-          ))}
+            ))}
+        </div>
+        <h2 className="py-2 text-center text-xl font-light">
+          Blog Collections
+        </h2>
+        <div className="space-y-2 py-5">
+          <GithubRepo repo="timqian/chinese-independent-blogs" />
+          <GithubRepo repo="tuna/blogroll" />
+        </div>
       </div>
-    </Main>
+    </div>
   );
 };
 
