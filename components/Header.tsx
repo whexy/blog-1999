54e7cce54b0dfb43f6e12f250a2cc9b90734ddaa
WenxuanShi <whexy@outlook.com>
2022-03-15 16:20:32 +0800

    post: mesh

--- components/Header.tsx
+++ components/Header.tsx
@@ -4,30 +4,27 @@ import NavigationView from "@/components/Navigation";
 
 const Header = () => {
   return (
-    <div>
-      <header className="fixed z-50 w-full bg-black-readable text-gray-400 print:hidden">
-        <div className="mx-auto flex max-w-6xl flex-row items-center justify-between space-x-8 px-5 py-4">
-          <Link href="/">
-            <a>
-              <div className="text-center font-mono text-xl text-white-readable sm:text-2xl">
-                <span className="text-white">
-                  {metadata.header.logo}
-                </span>
-                <sup className="text-red-500">
-                  {metadata.header.sublogo}
-                </sup>
-              </div>
-            </a>
-          </Link>
-          <div>
-            <div className="flex flex-row flex-nowrap items-center justify-center space-x-4">
-              <NavigationView />
+    <header className="sticky top-0 z-50 w-full bg-black-readable text-gray-400 print:hidden">
+      <div className="mx-auto flex max-w-6xl flex-row items-center justify-between space-x-8 px-5 py-4">
+        <Link href="/">
+          <a>
+            <div className="text-center font-mono text-xl text-white-readable sm:text-2xl">
+              <span className="text-white">
+                {metadata.header.logo}
+              </span>
+              <sup className="text-red-500">
+                {metadata.header.sublogo}
+              </sup>
             </div>
+          </a>
+        </Link>
+        <div>
+          <div className="flex flex-row flex-nowrap items-center justify-center space-x-4">
+            <NavigationView />
           </div>
         </div>
-      </header>
-      <div className="h-[65px]"></div>
-    </div>
+      </div>
+    </header>
   );
 };
 
