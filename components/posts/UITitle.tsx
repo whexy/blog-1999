4b59e248478662401537ad0bdf6c98a8bb34449c
WenxuanShi <whexy@outlook.com>
2022-03-16 12:39:21 +0800

    fix: UITitle wraps in small screen

--- components/posts/UITitle.tsx
+++ components/posts/UITitle.tsx
@@ -7,8 +7,8 @@ const UITitle = ({ title, emoji, subtitle, children }) => {
         <div className="h-4"></div>
         <div className="secondbg flex w-full items-center justify-between rounded-lg px-4 py-3 font-title shadow-lg">
           <h2 className="text-lg font-bold sm:text-xl">{title}</h2>
-          <div className="flex items-center space-x-2">
-            <span className="h-6 w-6">
+          <div className="flex items-center space-x-2 text-sm">
+            <span className="h-4 w-4">
               <Twemoji emoji={emoji} />
             </span>
             <span>{subtitle}</span>
