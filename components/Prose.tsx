48e5b64a7c53f91d17e2b584816ac58248a4f1d9
whexy <whexy@outlook.com>
2022-11-06 08:25:05 -0600

    footnote

--- components/Prose.tsx
+++ components/Prose.tsx
@@ -1,7 +1,7 @@
 const Prose = ({ children }) => {
   return (
     <div className="bg-white py-10 dark:bg-neutral-900 sm:mx-auto sm:rounded-lg sm:shadow-lg sm:dark:shadow-none md:max-w-4xl">
-      <div className="prose prose-neutral mx-auto overscroll-contain px-3 prose-headings:font-title prose-a:no-underline prose-a:shadow-[inset_0_-0.4rem_0_#eecfcf] prose-a:transition-all prose-a:duration-[0.25s] hover:prose-a:bg-[#eecfcf] dark:prose-invert dark:prose-a:shadow-[inset_0_-0.4rem_0_#7a2e2e] dark:hover:prose-a:bg-[#7a2e2e] md:prose-lg md:max-w-3xl">
+      <div className="prose prose-neutral mx-auto overscroll-contain px-3 prose-headings:font-title prose-a:no-underline prose-a:shadow-[inset_0_-0.4rem_0_#eecfcf] prose-a:transition-all prose-a:duration-[0.25s] hover:prose-a:bg-[#eecfcf] dark:prose-invert dark:prose-a:shadow-[inset_0_-0.4rem_0_#7a2e2e] dark:hover:prose-a:bg-[#7a2e2e] md:max-w-3xl md:prose-lg">
         {children}
       </div>
     </div>
