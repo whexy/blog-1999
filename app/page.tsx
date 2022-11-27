e641ed6d8e71e757e4985baf77251498164cf4ea
whexy <whexy@outlook.com>
2022-11-27 14:32:30 -0600

    Deep mourning for compatriots who died in the 11.24 fire in Urumqi

--- app/page.tsx
+++ app/page.tsx
@@ -14,18 +14,23 @@ export default function Page() {
   return (
     <>
       <WelcomeCard />
-      <Link href="/freedom">
-        <div
-          className={`my-10 ${zhiMangXing.className} text-center text-4xl`}
-        >
-          <p>
-            <span className="whitespace-nowrap">民主法治，</span>
-            <span className="whitespace-nowrap">表达自由，</span>
-            <span className="whitespace-nowrap">科学理性，</span>
-            <span className="whitespace-nowrap">融入世界。</span>
-          </p>
-        </div>
-      </Link>
+      <div className="py-4">
+        <h2 className="pl-4 pb-1 text-sm font-light opacity-80">
+          沉痛悼念 11.24 乌鲁木齐火灾罹难同胞
+        </h2>
+        <Link href="/freedom">
+          <div
+            className={`py-4 ${zhiMangXing.className} primary text-center text-4xl`}
+          >
+            <div className="flex flex-wrap justify-center gap-4">
+              <div className="whitespace-nowrap">民主法治</div>
+              <div className="whitespace-nowrap">表达自由</div>
+              <div className="whitespace-nowrap">科学理性</div>
+              <div className="whitespace-nowrap">融入世界</div>
+            </div>
+          </div>
+        </Link>
+      </div>
       <RecentPosts />
     </>
   );
