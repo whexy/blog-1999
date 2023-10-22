f081060c3760bfdabee243e1242c43ea46c684fe
whexy <whexy@outlook.com>
2023-10-22 09:42:11 -0500

    series with modern style

--- components/UI/Blog/Series.tsx
+++ components/UI/Blog/Series.tsx
@@ -20,22 +20,21 @@ const Series = ({ slug, series }) => {
 
   return (
     <div>
-      <div className="not-prose relative m-4 break-inside-avoid-page rounded-lg border-2 border-violet-200/80 bg-violet-200/10 p-4 font-sans">
+      <div className="not-prose relative m-4 break-inside-avoid-page rounded-lg border-2 border-violet-200/80 bg-violet-200/5 p-4 font-sans">
         <div className="absolute left-0 top-0 flex items-center justify-center space-x-2 border-b border-r border-violet-300/80 bg-violet-300/10 px-2 font-bold">
           <BookOpenIcon className="h-6 w-6" />
           <div>{series}</div>
         </div>
-        <div className="pb-4 pt-6 text-sm">
-          This article is part of a series.
-        </div>
-        <ul className="list-inside list-decimal">
+        <ul className="list-inside list-decimal pt-6">
           {seriesPosts.map(({ slug, title }) => (
             <li
               key={slug}
-              className={`hover:underline ${
-                slug === thisSlug && "font-bold underline"
-              }`}>
-              <Link href={`/posts/${slug}`}>{title}</Link>
+              className={`${slug === thisSlug && "font-bold"}`}>
+              <Link
+                href={`/posts/${slug}`}
+                className="rounded-sm p-1 transition-all hover:bg-violet-800/5">
+                {title}
+              </Link>
             </li>
           ))}
         </ul>
